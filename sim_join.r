ca7dce1e4b313a3f3b31b3ed3527122ae376dc35
Marcello Filgueiras <53457944+marcellofilgueiras@users.noreply.github.com>
2021-07-07 10:28:37 -0300

    sim_join #2
    Adiciona gráfico de linha

--- sim_join.r
+++ sim_join.r
@@ -12,6 +12,8 @@ sim_join_2018_2020 <- full_join(sim_bdd_2018_filtro_causa_basica,
 
 # Visualizando ------------------------------------------------------------
 
+#Número de Mortes por Trem Gástrico por mês
+
 
 sim_join_2018_2020 %>%
   filter(causa_basica %in% cids_vetor) %>% 
@@ -29,7 +31,7 @@ sim_join_2018_2020 %>%
                                                      size = 10, angle = 90),
                           plot.title = element_text(size = 16, face= "bold"))
 
-#"Número de Mortes por Hérnia por mês em 2020"
+#número de Mortes por Hérnia por mês
 
 sim_join_2018_2020 %>%
   filter(causa_basica %in% hernias_vetor) %>% 
@@ -48,3 +50,22 @@ sim_join_2018_2020 %>%
         plot.title = element_text(size = 16, face= "bold"))
 
 
+#número de Mortes por Hérnia por mês em Linha
+
+
+sim_join_2018_2020 %>%
+  filter(causa_basica %in% hernias_vetor) %>% 
+  mutate(mes = paste(str_sub(data_obito, 1,7),01, sep="-")%>%
+           lubridate::ymd())%>%
+  group_by(mes) %>%
+  count(causa_basica)%>%
+  tsibble::as_tsibble(index=mes, key = causa_basica) %>%
+  ggplot(aes(x=mes, y=n, colour=causa_basica, group= causa_basica )) + 
+  geom_smooth(size=1) +
+  labs(title= "Número de Mortes por Hérnia por mês",
+       subtitle = "Dados de 2018 - 2020",
+       caption = "Fonte: Sistema Informatizado de Mortalidade (SIM) - Open Data Sus e Base dos Dados") +
+  scale_x_date( breaks= "1 month") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(face = "bold", size = 10, angle = 90),
+        plot.title = element_text(size = 16, face= "bold"))
